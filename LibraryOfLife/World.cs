550fa44cf678e458e9ab59c22bdc063845fe02b6
Daniel Scott <stonedcrow@gmail.com>
2012-03-25 14:07:33 +0200

    Began work on the World class

---
+++ LibraryOfLife/World.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace LibraryOfLife
+{
+    public class World
+    {
+        public int CellCount
+        {
+            get { throw new NotImplementedException(); }
+            set { throw new NotImplementedException(); }
+        }
+    }
+}
--- TestOfLife/TestWorld.cs
+++ TestOfLife/TestWorld.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using Conway.LibraryOfLife;
+using LibraryOfLife;
 using NUnit.Framework;
 
 namespace TestOfLife
@@ -9,5 +11,17 @@ namespace TestOfLife
     [TestFixture]
     public class TestWorld
     {
+        
+        [Test]
+        public void TestEmptyWorld()
+        {
+            //Given
+            var world = new World();
+            //With
+                //nothing
+            //Check
+            Assert.AreEqual(0, world.CellCount);
+        }
+
     }
 }
